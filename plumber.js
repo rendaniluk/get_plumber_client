1e634e513fabee1913e759ef341c352b4e6097ea
rendaniluk <lukhwa11582924@gmail.com>
2017-12-06 00:02:58 +0200

    plumber_front-end

---
+++ plumber.js
@@ -0,0 +1,16 @@
+(function() {
+
+  $(".addStock").submit(function(e) {
+    var url = "http://localhost:5000/api/plumber/slot"; // the script where you handle the form input.
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: $(".addPlumber").serialize(), // serializes the form's elements.
+      success: function(data) {
+        console.log(data);
+
+      }
+    });
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+  });
+})();
